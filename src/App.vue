36a940a579bbc5c9576221d0bdd3af045c020bc4
liuziting <57311725+liu-ziting@users.noreply.github.com>
2024-05-23 10:23:36 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,6 @@
 <template>
     <div>
-        <h1>Hi.</h1>
-        <p style="font-weight: 500; font-size: 2.2em">My name is Liu Ziting.</p>
-        <p>
-            作为一名有着丰富创造力的前端开发工程师，我对于探索互联网的未知角落充满热情。近三年来，我热衷于AI领域，享受将想法通过代码实现，并成功部署上线的过程。
-        </p>
-        <p>我熟悉使用VUE, JavaScript, Python以及一点React和NextJS，并且对Astro和Tailwindcss框架情有独钟。</p>
-        <p>
-            在过去的两年里，我开发了一系列与AI相关的网站项目。例如，一个汇集AI产品的平台：<a
-                href="https://www.smartai.wtf/"
-                target="_blank"
-                >SmartAI</a
-            >，一个展现sora视频创意的网站：<a href="https://www.soradesign.top/" target="_blank">SoraDesign</a
-            >，以及一个利用Gemini开发的，能够通过上传照片来解析宠物想法的有趣工具：<a
-                href="https://iknowwhatyourcatisthinking.netlify.app/"
-                target="_blank"
-                >我知道你的宠物在想什么</a
-            >。
-        </p>
-        <p>
-            此外，我还会在<a href="https://juejin.cn/user/870468937854734/posts" target="_blank">Juejin</a
-            >分享我对技术的见解和写作。
-        </p>
-        <p>
-            我的更多项目可以在<a href="https://github.com/liu-ziting" target="_blank">GitHub</a
-            >上找到。我喜欢在业余时间写作教程、分享技术文章或参与开源项目的开发。欢迎Follow我，了解我的最新动态。
-        </p>
-        <p>或加入我的<a href="https://discord.com/invite/mc6hPwPqZ7" target="_blank">Discord</a>社群一起沟通交流~</p>
-        <p>除了编程，我还对骑行和滑板充满热情。</p>
+        <h1>GPT-4o.</h1>
     </div>
 </template>
 
